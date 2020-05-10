0962fc72225e06f092ddbe701cb9e522e3cf30c8
Yayan He'eh <garanganmanis@yandex.com>
2020-05-10 09:51:10 +0700

    first commit

---
+++ src/layout/dashboard/ContentFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid d-flex flex-wrap justify-content-between">
+      <nav>
+        <ul>
+          <li>
+            <router-link :to="{path:'/admin'}">Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright d-flex flex-wrap">
+        2020 - 
+        
+       <a href="https://www.tiaweb.github.io" target="_blank">&nbsp; Tiaweb.</a>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
